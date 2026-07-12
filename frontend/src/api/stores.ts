ff2f5b86deb2f35298ea760df321acabf2a88b99
sujith <syjithkpy@gmail.com>
2026-07-12 22:32:06 +0530

    feat: add update/delete and location parameter to stores API

--- frontend/src/api/stores.ts
+++ frontend/src/api/stores.ts
@@ -3,6 +3,7 @@ import api from './axios';
 export interface StoreType {
   _id: string;
   name: string;
+  location: string;
 }
 
 export const getStores = async (): Promise<StoreType[]> => {
@@ -10,7 +11,17 @@ export const getStores = async (): Promise<StoreType[]> => {
   return res.data?.data
 };
 
-export const createStore = async (newStore: { name: string ,location:string}): Promise<StoreType> => {
+export const createStore = async (newStore: { name: string; location: string }): Promise<StoreType> => {
   const res = await api.post('/store', newStore);
   return res.data?.data
+};
+
+export const updateStore = async (id: string, updatedStore: Partial<StoreType>): Promise<StoreType> => {
+  const res = await api.put(`/store/${id}`, updatedStore);
+  return res.data?.data;
+};
+
+export const deleteStore = async (id: string): Promise<any> => {
+  const res = await api.delete(`/store/${id}`);
+  return res.data;
 };
